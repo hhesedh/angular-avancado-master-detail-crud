441ea65e53ce6990ab6ec7f10e31f7015e61fa48
fabio <oliveira_fabio@id.uff.br>
2019-03-02 22:03:45 -0300

    refatoracao de codigo

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Category } from "../shared/category.model";
 import { CategoryService } from "../shared/category.service";
-import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-category-list',
